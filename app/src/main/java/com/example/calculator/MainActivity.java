3c7d6d28018fd447be8128ecf5cb84c51e622ba1
Akos Becsey <becsey.a@gmail.com>
2021-09-13 19:40:32 +0200

    Add Event Listener

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onButtonClick(View view) {
+        Button b = (Button) view;
+
+        Toast.makeText(this, "Btn " + b.getText() + " pressed", Toast.LENGTH_SHORT).show();
     }
 }
